d74687c5e1fe3379d65dd8c42dfc1b917620c97b
Alexander Khranovskiy <alex_khranovskiy@ukr.net>
2024-01-23 11:11:20 +0200

    Fixed PositionService

--- app/Services/PositionService.php
+++ app/Services/PositionService.php
@@ -4,12 +4,13 @@ namespace App\Services;
 
 use App\Models\Position;
 use App\Traits\ResponseTrait;
+use Illuminate\Http\JsonResponse;
 
 class PositionService
 {
     use ResponseTrait;
 
-    public function getAll()
+    public function getAll(): JsonResponse
     {
         $positions = Position::all(['id', 'name']);
 
