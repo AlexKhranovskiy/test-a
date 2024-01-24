1eaf8506b8cb42b2f4f6b4ea49c5778e66fae1cd
Alexander Khranovskiy <alex_khranovskiy@ukr.net>
2024-01-24 19:58:36 +0200

    Added annotations

--- app/Services/TinyPngService.php
+++ app/Services/TinyPngService.php
@@ -8,9 +8,16 @@ class TinyPngService
 {
     use LoggerTrait;
 
+    /** Resizes and handles image file using thumb algorithm.
+     * @param string $fileDirectory
+     * @param string $fileName
+     * @param int $width
+     * @param int $height
+     * @return void
+     */
     public function fitPhotoByThumbAlgorithm(
         string $fileDirectory, string $fileName, int $width = 70, int $height = 70
-    )
+    ): void
     {
         $this->writeInfoLog('fitPhotoByThumbAlgorithm', [
             '$fileDirectory' => $fileDirectory,
@@ -30,107 +37,19 @@ class TinyPngService
         ]);
     }
 
-    public function convertToJpeg(string $fileDirectory, string $fileName)
+    /** Converts format of image file to jpeg.
+     * @param string $fileDirectory
+     * @param string $fileName
+     * @return string
+     */
+    public function convertToJpeg(string $fileDirectory, string $fileName): string
     {
-        $this->writeInfoLog('convertToJpeg', [
-            '$fileDirectory' => $fileDirectory,
-            '$fileName' => $fileName,
-        ]);
-
         \Tinify\setKey(env('TINY_PNG_SERVICE_API_KEY', 'lpFY4p5k4bzqgtDV8H3pBTg1h748SyzY'));
         $source = \Tinify\fromFile($fileDirectory . '/' . $fileName);
         $converted = $source->convert(array("type" => ["image/jpeg"]));
         $extension = $converted->result()->extension();
         $converted->toFile($fileDirectory . '/' . $fileName);
 
-        $this->writeInfoLog('convertToJpeg ended', [
-            '$fileDirectory' => $fileDirectory,
-            '$fileName' => $fileName,
-        ]);
         return $extension;
     }
-
-//    public function fitPhotoByThumbAlgorithmConvertToJpg(string $fileDirectory, string $sourceFileName, int $width = 70, int $height = 70)
-//    {
-//        \Tinify\setKey(env('TINY_PNG_SERVICE_API_KEY', 'lpFY4p5k4bzqgtDV8H3pBTg1h748SyzY'));
-//        $this->writeInfoLog('Called fitPhotoByThumbAlgorithmConvertToJpg', [
-//            'fileDirectory' => $fileDirectory,
-//            'sourceFileName' => $sourceFileName
-//        ]);
-//        $fileNameItems = explode('.', $sourceFileName);
-//        $fileName = $fileNameItems[0];
-//        $fileExtension = $fileNameItems[1];
-//
-//        $this->writeInfoLog('Taken file params', [
-//            'fileName' => $fileName,
-//            'fileExtension' => $fileExtension
-//        ]);
-//
-//        $lowerFileExtension = strtolower($fileExtension);
-//
-//        $this->writeInfoLog('strtolower of file extension', [
-//            'lowerFileExtension' => $lowerFileExtension
-//        ]);
-//
-//        if ($lowerFileExtension != 'jpg' && $lowerFileExtension != 'jpeg') {
-//
-//            $this->writeInfoLog('lowerFileExtension != jpg and != jpeg', [
-//                'lowerFileExtension' => $lowerFileExtension
-//            ]);
-//
-//            $this->writeInfoLog('File params now', [
-//                'fileDirectory' => $fileDirectory,
-//                'sourceFileName' => $sourceFileName,
-//            ]);
-//
-//            $source = \Tinify\fromFile($fileDirectory . '/' .$sourceFileName);
-//            $converted = $source->convert(array("type" => ["image/jpeg"]));
-//            $extension = $converted->result()->extension();
-//            $converted->toFile($fileDirectory . '/' . $fileName . '.' . $extension);
-//
-//            $this->writeInfoLog('Converted', [
-//                'saved with' => [
-//                    'fileDirectory' => $fileDirectory,
-//                    'fileName' => $fileName,
-//                    'extension' => $extension,
-//                ]
-//            ]);
-//
-//            unlink($sourceFileName);
-//
-//            $this->writeInfoLog('Removed file', [
-//                'sourceFileName' => $sourceFileName,
-//            ]);
-//
-//            $sourceFileName = $fileName . '.' . $extension;
-//
-//            $this->writeInfoLog('sourceFileName now', [
-//                'sourceFileName' => $sourceFileName,
-//            ]);
-//        }
-//
-//        $this->writeInfoLog('File params now', [
-//            'fileDirectory' => $fileDirectory,
-//            'sourceFileName' => $sourceFileName,
-//        ]);
-//
-//        $source = \Tinify\fromFile($fileDirectory . '/' .$sourceFileName);
-//        $resized = $source->resize(array(
-//            "method" => "thumb",
-//            "width" => $width,
-//            "height" => $height
-//        ));
-//        $resized->toFile($fileDirectory . '/' .$sourceFileName);
-//
-//        $this->writeInfoLog('Resized and saved with', [
-//            'fileDirectory' => $fileDirectory,
-//            'sourceFileName' => $sourceFileName,
-//        ]);
-//
-//        $this->writeInfoLog('Returned sourceFileName', [
-//            'sourceFileName' => $sourceFileName,
-//        ]);
-//
-//        return $sourceFileName;
-//    }
 }
